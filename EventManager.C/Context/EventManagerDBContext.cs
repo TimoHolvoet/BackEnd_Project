ecf411ece9c57d4df4ee8ed1e7e9e3c38599ccb8
TimoHolvoet <timo.holvoet@student.howest.be>
2019-04-29 12:36:06 +0200

    init van EventManagerDbContext

---
+++ EventManager.C/Context/EventManagerDBContext.cs
@@ -0,0 +1,20 @@
+﻿using EventManager.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManager.Core.Context
+{
+    class EventManagerDBContext : DbContext
+    {
+        public EventManagerDBContext(DbContextOptions<EventManagerDBContext> options)
+            : base(options)
+        {
+
+        }
+        //init tabellen
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
